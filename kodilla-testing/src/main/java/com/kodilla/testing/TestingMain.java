18913b8297bec501997262cf785da2e64b0880f6
Galigad <artur.szuberla@gmail.com>
2018-01-09 23:30:00 +0100

    new test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+            Calculator calculator = new Calculator();
+
+            int adding = calculator.add(5,4);
+            int substracting = calculator.substract(3,7);
+
+            System.out.println("Test - pierwszy test jednostkowy:");
+            if (5 + 4 == adding && substracting == 3-7) {
+                System.out.println("Adding OK");
+            }
+            else {
+                System.out.println("Error!");
+            }
+            if (substracting == 3-7) {
+                System.out.println("Substracting OK");
+            }
+            else {
+                System.out.println("Error!");
+            }
     }
 }
