1d2d0221ae8f7da38ed7f1137d5435e0fd6e139b
Galigad <artur.szuberla@gmail.com>
2018-03-14 22:01:35 +0100

    Module 13

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,14 +1,19 @@
 package com.kodilla.hibernate.tasklist;
 
+import com.kodilla.hibernate.task.Task;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "TASKLISTS")
 public final class TaskList {
-    private int ID;
+    private int Id;
     private String listName;
     private String description;
+    private List<Task> tasks = new ArrayList<>();
 
 
     public TaskList() {
@@ -16,7 +21,7 @@ public final class TaskList {
     }
 
     public TaskList(String listName, String description) {
-        this.ID = ID;
+        this.Id = Id;
         this.listName = listName;
         this.description = description;
     }
@@ -24,13 +29,13 @@ public final class TaskList {
     @Id
     @GeneratedValue
     @NotNull
-    @Column(name = "ID", unique = true)
-    public int getID() {
-        return ID;
+    @Column(name = "Id", unique = true)
+    public int getId() {
+        return Id;
     }
 
-    public void setID(int ID) {
-        this.ID = ID;
+    public void setId(int id) {
+        this.Id = id;
     }
 
     @Column(name = "LIST_NAME")
@@ -48,5 +53,19 @@ public final class TaskList {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    @OneToMany(
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 }
